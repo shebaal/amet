872f1c6e75719d84533642e63e5247bf298ea504
ايناس حميد القعادي <80882475+EnasHameedQ@users.noreply.github.com>
2021-11-26 18:21:24 +0300

    need to change img

---
+++ enas.vue
@@ -0,0 +1,342 @@
+<template>
+<div
+    class="
+      px-4
+      py-12
+      md:mx-12
+      lg:mx-40
+      2xl:mx-80
+      overflow-hidden
+      bg-primary-100
+    "
+  >
+    <div class="c relative">
+      <div class="bg-white rounded-full w-72 h-72 relative shadow-md">
+        <p class="top-1/2 left-1/2 absolute tran font-Montserrat-Bold text-lg text-center">
+         THE CONS & POOSS
+        </p>
+      </div>
+
+      <div
+        class="bg-primary-100 rounded-full w-28 h-28 top-1/3 left-1/3 absolute"
+      ></div>
+
+      <div
+        class="
+          bg-white
+          rounded-full
+          w-24
+          h-24
+          top-28
+          left-1/4
+          absolute
+          shadow-md
+        "
+      ></div>
+
+      <div
+        class="
+          bg-white
+          rounded-full
+          w-16
+          h-16
+          top-32
+          left-28
+          absolute
+          shadow-md
+        "
+      ></div>
+
+      <div
+        class="
+          bg-white
+          rounded-full
+          w-24
+          h-24
+          top-28
+          right-1/4
+          absolute
+          shadow-md
+        "
+      ></div>
+
+      <div
+        class="
+          bg-white
+          rounded-full
+          w-16
+          h-16
+          top-32
+          right-28
+          absolute
+          shadow-md
+        "
+      ></div>
+    </div>
+
+    <div
+      class="
+        c
+        w-full
+        my-12
+        p-12
+        bg-white
+        shadow-sm
+        rounded
+        flex
+        lg:flex-row
+        md:flex-col
+        flex-col
+        sm:flex-col
+      "
+    >
+      
+
+      <div class="flex-1">
+        <img src="../assets/img/tail2.png" />
+         <a class="text-red-400 " href="https://tailwind-converter.netlify.app/" > https://tailwind-converter.netlify.app/</a>
+      </div>
+    </div>
+
+
+       <div>
+    </div>
+  </div>
+  <div
+    class="
+      px-4
+      py-12
+      md:mx-12
+      lg:mx-40
+      2xl:mx-80
+      overflow-hidden
+      bg-primary-100
+    "
+  >
+    <div class="c relative">
+      <div class="bg-white rounded-full w-72 h-72 relative shadow-md">
+        <p class="top-1/2 left-1/2 absolute tran font-Montserrat-Bold text-lg">
+          How to install Tailwind Css ?
+        </p>
+      </div>
+
+      <div
+        class="bg-primary-100 rounded-full w-28 h-28 top-1/3 left-1/3 absolute"
+      ></div>
+
+      <div
+        class="
+          bg-white
+          rounded-full
+          w-24
+          h-24
+          top-28
+          left-1/4
+          absolute
+          shadow-md
+        "
+      ></div>
+
+      <div
+        class="
+          bg-white
+          rounded-full
+          w-16
+          h-16
+          top-32
+          left-28
+          absolute
+          shadow-md
+        "
+      ></div>
+
+      <div
+        class="
+          bg-white
+          rounded-full
+          w-24
+          h-24
+          top-28
+          right-1/4
+          absolute
+          shadow-md
+        "
+      ></div>
+
+      <div
+        class="
+          bg-white
+          rounded-full
+          w-16
+          h-16
+          top-32
+          right-28
+          absolute
+          shadow-md
+        "
+      ></div>
+    </div>
+
+    <div
+      class="
+        c
+        w-full
+        my-12
+        p-12
+        bg-white
+        shadow-sm
+        rounded
+        flex
+        lg:flex-row
+        md:flex-col
+        flex-col
+        sm:flex-col
+      "
+    >
+      <div class="flex-1">
+        <div class="text-xl py-4">Docomintation</div>
+        <p class="text-base font-Montserrat-Regular my-2">
+            Installing Tailwind CSS can be a slightly different process
+             depending on what other frameworks/tools you’re using, so we’ve put together a bunch of guides that cover popular setups.
+       so vist the site 
+       <a class="text-red-400 " href="https://tailwindcss.com/docs/installation" > https://tailwindcss.com/docs/installation</a>
+       and chose frameworks/tools you’re using. </p> 
+       
+      </div>
+
+      <div class="flex-1">
+        <img src="../assets/img/tail2.png" />
+      </div>
+    </div>
+
+
+       <div>
+    </div>
+  </div>
+  <!--///////////////////////////////////////////-->
+
+   <div
+    class="
+      px-4
+      py-12
+      md:mx-12
+      lg:mx-40
+      2xl:mx-80
+      overflow-hidden
+      bg-primary-100
+    "
+  >
+    <div class="c relative">
+      <div class="bg-white rounded-full w-72 h-72 relative shadow-md">
+        <p class="top-1/2 left-1/2 absolute tran font-Montserrat-Bold text-lg">
+          component
+        </p>
+      </div>
+
+      <div
+        class="bg-primary-100 rounded-full w-28 h-28 top-1/3 left-1/3 absolute"
+      ></div>
+
+      <div
+        class="
+          bg-white
+          rounded-full
+          w-24
+          h-24
+          top-28
+          left-1/4
+          absolute
+          shadow-md
+        "
+      ></div>
+
+      <div
+        class="
+          bg-white
+          rounded-full
+          w-16
+          h-16
+          top-32
+          left-28
+          absolute
+          shadow-md
+        "
+      ></div>
+
+      <div
+        class="
+          bg-white
+          rounded-full
+          w-24
+          h-24
+          top-28
+          right-1/4
+          absolute
+          shadow-md
+        "
+      ></div>
+
+      <div
+        class="
+          bg-white
+          rounded-full
+          w-16
+          h-16
+          top-32
+          right-28
+          absolute
+          shadow-md
+        "
+      ></div>
+    </div>
+
+    <div
+      class="
+        c
+        w-full
+        my-12
+        p-12
+        bg-white
+        shadow-sm
+        rounded
+        flex
+        lg:flex-row
+        md:flex-col
+        flex-col
+        sm:flex-col
+      "
+    >
+      <div class="flex-1">
+        <div class="text-xl py-4">Components </div>
+        <p class="text-base font-Montserrat-Regular">
+         Over 400+ professionally designed, fully responsive, expertly crafted component examples you can drop into your Tailwind projects and customize to your heart’s content. 
+         Get started by checking out our free preview components, or browsing all of the examples in the categories you're most curious about.
+        </p>
+        <a class="text-red-400 " href="https://tailwindui.com/#components" > https://tailwindui.com/#components</a>
+      </div>
+
+      <div class="flex-1">
+        <img src="../assets/img/tail1.png" class="rounded-lg shadow-lg " />
+      </div>
+    </div>
+
+
+       <div>
+    </div>
+  </div>
+</template>
+
+
+
+
+
+
+
+<style>
+.tran {
+  transform: translate(-50%, -50%);
+}
+.c {
+  text-align: -webkit-center;
+}
+</style>
