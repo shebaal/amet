16989c009990911a68babe1e9ce0df9b9e72e294
shebaal <alshamery.sheba@gmail.com>
2021-11-26 07:55:01 -0800

    finsh tailwind ui

--- src/components/sheba.vue
+++ src/components/sheba.vue
@@ -89,13 +89,13 @@
         flex-col
         sm:flex-col
       "
+      v-for="i in sections" :key="i"
     >
-      <div class="flex-1">
-        <div class="text-xl py-4">Theme</div>
+      <div class="flex-1" >
+        <div class="text-xl py-4">{{i.title}}</div>
         <p class="text-base font-Montserrat-Regular">
-          The theme section is where you define your color palette, fonts, type
-          scale, border sizes, breakpoints — anything related to the visual
-          design of your site.
+            {{i.para}}
+        
         </p>
       </div>
 
@@ -105,36 +105,96 @@
     </div>
 
 
-       <div
-      class="
-        c
+
+
+    <div class="c relative">
+      <div class="bg-white rounded-full w-72 h-72 relative shadow-md">
+        <p class="top-1/2 left-1/2 absolute tran font-Montserrat-Bold text-lg">
+          Tailwind & UI
+        </p>
+      </div>
+
+      <div
+        class="bg-primary-100 rounded-full w-28 h-28 top-1/3 left-1/3 absolute"
+      ></div>
+
+      <div
+        class="
+          bg-white
+          rounded-full
+          w-24
+          h-24
+          top-28
+          left-1/4
+          absolute
+          shadow-md
+        "
+      ></div>
+
+      <div
+        class="
+          bg-white
+          rounded-full
+          w-16
+          h-16
+          top-32
+          left-28
+          absolute
+          shadow-md
+        "
+      ></div>
+
+      <div
+        class="
+          bg-white
+          rounded-full
+          w-24
+          h-24
+          top-28
+          right-1/4
+          absolute
+          shadow-md
+        "
+      ></div>
+
+      <div
+        class="
+          bg-white
+          rounded-full
+          w-16
+          h-16
+          top-32
+          right-28
+          absolute
+          shadow-md
+        "
+      ></div>
+    </div>
+
+
+<div class="    
         w-full
         my-12
         p-12
         bg-white
         shadow-sm
-        rounded
-        flex
-        lg:flex-row
-        md:flex-col
-        flex-col
-        sm:flex-col
-      "
-    >
-      <div class="flex-1">
-        <div class="text-xl py-4">Padding scale</div>
-        <p class="text-base font-Montserrat-Regular">
-         By default, Tailwind provides 19 fixed padding utilities for each side .
-        </p>
-      </div>
+        rounded">
+    <p class="text-xl font-extrabold text-center">Colors</p>
+    <img src="../assets/img/swatches-04.png" />
+</div>
 
-      <div class="flex-1">
-        <img src="../assets/img/tail4.png" class="rounded-lg shadow-lg " />
-      </div>
-    </div>
+<div class="c    
+        w-full
+        my-12
+        p-12
+        bg-white
+        shadow-sm
+        rounded">
+    <p class="text-xl font-extrabold text-center my-4">How to?</p>
+    <img src="../assets/img/ui-design-mistakes-7.jpg" />
+</div>
 
-
-
+    
           <div
       class="
         c
@@ -152,57 +212,58 @@
       "
     >
       <div class="flex-1">
-        <div class="text-xl py-4 ">
-            Margin scale</div>
-        <p class="text-base font-Montserrat-Regular">
-        By default, Tailwind provides 20 margin utilities for each side .
-        </p>
+        <div class="text-xl  ">
+            UI </div>
+       
+        <img src="../assets/img/tail7.png" class="rounded-lg shadow-lg " />
       </div>
 
       <div class="flex-1">
-        <img src="../assets/img/tail4.png" class="rounded-lg shadow-lg " />
+           <div class="text-xl  ">
+            Tailwind CSS</div>
+        <img src="../assets/img/tail8.png" class="rounded-lg shadow-lg " />
       </div>
     </div>
 
 
-         <div
-      class="
-        c
+    <div class="   c 
         w-full
         my-12
         p-12
         bg-white
         shadow-sm
-        rounded
-        flex
-        lg:flex-row
-        md:flex-col
-        flex-col
-        sm:flex-col
-      "
-    >
-      <div class="flex-1">
-        <div class="text-xl py-4 ">
-            Width scale</div>
-        <p class="text-base font-Montserrat-Regular">
-            By default, Tailwind’s width scale is a combination of the default spacing scale as well as some additional values specific to widths.
-        </p>
-      </div>
-
-      <div class="flex-1">
-        <img src="../assets/img/tail6.png" class="rounded-lg shadow-lg " />
-      </div>
-    </div>
+        rounded">
+    <p class="text-xl font-extrabold text-center">Spacing</p>
+    <img src="../assets/img/tail9.png" />
+</div>
 
 
-
-    
   </div>
 </template>
 
 
 
 
+<script>
+
+export default {
+    data(){
+        return{
+            sections:[
+                {title:'Theme',
+                para:'  The theme section is where you define your color palette, fonts, type scale, border sizes, breakpoints — anything related to the visual design of your site.'},
+             {title:'Padding scale',
+                para:'  By default, Tailwind provides 19 fixed padding utilities for each side .'},
+             {title:'Margin scale',
+                para:' By default, Tailwind provides 20 margin utilities for each side .'},
+             {title:'Width scale',
+                para:' By default, Tailwind’s width scale is a combination of the default spacing scale as well as some additional values specific to widths.'},
+           
+           ]
+        }
+    }
+}
+</script>
 
 
 
